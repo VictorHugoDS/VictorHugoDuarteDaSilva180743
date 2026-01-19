95fbe7e88045a3f7bd616acb3e36f8e3ff827b00
VictorHugoDS <victorhugod.s@hotmail.com>
2026-01-19 10:32:20 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/hamix/HamixApplication.java
@@ -0,0 +1,13 @@
+package br.com.hamix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hamix/HamixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hamix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
